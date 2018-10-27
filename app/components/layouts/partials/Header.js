26943bfebcb9537a97df48ef392e316ab6371f9f
Prem Acharya <prem.acharya@hotmail.com>
2018-10-27 19:55:58 +1100

    layout added

---
+++ app/components/layouts/partials/Header.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>{this.props.title || 'Backpacker Courrier'}</title>
+
+          <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
+        </Head>
+        <div>
+          <Navbar
+            color="info"
+            light
+            className="navbar navbar-expand-md pt-3 pb-3"
+          >
+            <Link prefetch href="/">
+              <NavbarBrand href="/">BackPacker Courier</NavbarBrand>
+            </Link>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Link href="/earn-money/">
+                    <a className="nav-link">Earn Mobey</a>
+                  </Link>
+                </NavItem>
+
+                <NavItem>
+                  <Link href="/sign-up/">
+                    <a className="nav-link">Sign UP</a>
+                  </Link>
+                </NavItem>
+
+                <NavItem>
+                  <Link href="/login/">
+                    <a className="nav-link">Login</a>
+                  </Link>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      </React.Fragment>
+    )
+  }
+}
