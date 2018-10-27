858dcf024535c5b0aaf4e03e8db57c552ead0ca0
Prem Acharya <prem.acharya@hotmail.com>
2018-10-27 11:51:12 +1100

    nodemon hot-load added

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React from "react";
-import { Alert } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import { Alert } from 'reactstrap'
 
 export default () => (
   <div>
-    Welcome to next.js!
+    Welcome to next.js
     <Alert color="primary">This is a primary alert — check it out!</Alert>
   </div>
-);
+)
