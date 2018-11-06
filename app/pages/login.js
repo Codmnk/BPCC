8112157fe7d20d629073eed741ac4991dad2eed4
Prem Acharya <prem.acharya@hotmail.com>
2018-11-07 01:07:29 +1100

    client side validation added to signin form

--- app/pages/login.js
+++ app/pages/login.js
@@ -7,37 +7,96 @@ import {
   FormGroup,
   Label,
   Input,
-  FormText,
+  Alert,
   Jumbotron,
 } from 'reactstrap'
 import './loginNregister.css'
+
+const initialState = {
+  uEmail: '',
+  uPassword: '',
+  uEmailErr: '',
+  uPasswordErr: '',
+  isMailSent: false,
+}
+
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+)
+
 export default class login extends React.Component {
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
+  handleChange = e => {
+    e.preventDefault()
+
+    const { name, value } = e.target
+    let emailErr = ''
+    let passErr = ''
+
+    switch (name) {
+      case 'uEmail':
+        emailErr = emailRegex.test(value) ? '' : 'Invalid Email!'
+        break
+
+      case 'uPassword':
+        passErr =
+          value.length < 6 ? 'Password must have minimum of 6 characters' : ''
+        break
+
+      default:
+        return
+    }
+    this.setState({
+      uEmailErr: emailErr,
+      uPasswordErr: passErr,
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { uEmailErr, uPasswordErr } = this.state
+
+    //IF NO ERROR, SEND EMAIL
+    !uEmailErr && !uPasswordErr
+      ? console.log('get jwt or unauthorize')
+      : console.log(`Failed to send`)
+  }
+
   render() {
+    const { uEmailErr, uPasswordErr } = this.state
     return (
       <SingleColLayout>
         <div className="container">
           <Jumbotron className="rounded-5 loginForm">
             <h2 className="display-5 font-weight-bold">Login</h2>
             <hr className="my-2" />
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <FormGroup>
                 <Label for="userEmail">Email</Label>
                 <Input
+                  onChange={this.handleChange}
                   type="email"
                   name="uEmail"
                   id="userEmail"
                   placeholder="Your Email"
                   required
                 />
+                {uEmailErr && <Alert color="danger"> {uEmailErr}</Alert>}
               </FormGroup>
               <FormGroup>
                 <Label for="userPassword">Password</Label>
                 <Input
+                  onChange={this.handleChange}
                   type="password"
                   name="uPassword"
                   id="userPassword"
                   placeholder="Password"
                 />
+                {uPasswordErr && <Alert color="danger">{uPasswordErr}</Alert>}
               </FormGroup>
 
               <FormGroup check>
